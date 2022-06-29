accbaabbc95b495a098d0e6cf49c6d25f0e571a9
zhp2672961671 <2672961671@qq.com>
2022-06-29 18:26:17 +0800

    mysql

--- my-server/src/app.module.ts
+++ my-server/src/app.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
+// import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Log4jsModule } from './log4js';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
+import { ProjModule } from './mpgame/proj.module';
 
 @Module({
-  
-  imports: [ Log4jsModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+
+  imports: [ Log4jsModule.forRoot(),
+    CacheModule.register({    // 导入缓存模块
+      isGlobal: true,
+      ttl: 0,
+    }),
+    // .forRoot()调用初始化调度器并且注册在你应用中任何声明的cron jobs,timeouts和intervals。
+    ScheduleModule.forRoot(), // 导入计划模块
+    ConfigModule.forRoot({     //导入配置模块
+      envFilePath: '.env.sample',
+    }),
+    ProjModule,
+  ],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
---
+++ my-server/src/mpgame/proj.module.ts
@@ -0,0 +1,52 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+    imports: [
+      TypeOrmModule.forRootAsync({
+        // 异步加载，此时配置器完成注入
+        useFactory: () => ({
+          type: 'mysql', // 数据库类型
+          entities: [    // 数据表实体 扫描项目路劲中.entity文件
+            "dist/blockchain/**/*.entity{.ts,.js}",
+            "dist/mpgame/**/*.entity{.ts,.js}"
+          ],
+          host: process.env.DB_HOST || 'localhost', // 主机，默认为localhost
+          port: Number(process.env.DB_PORT) || 3306, // 端口号
+          username: process.env.DB_USER || 'root',   // 用户名
+          password: process.env.DB_PASS || 'root', // 密码
+          database: process.env.DB_NAME || 'test', //数据库名
+          timezone: '+08:00', //服务器上配置的时区
+          synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+        }),
+        // useFactory: () => ({
+        //   type: 'mysql', // 数据库类型
+        //   entities: [    // 数据表实体 扫描项目路劲中.entity文件
+        //     // "dist/blockchain/**/*.entity{.ts,.js}",
+        //     // "dist/mpgame/**/*.entity{.ts,.js}"
+        //   ],
+        //   host:'localhost', // 主机，默认为localhost
+        //   port:  3306, // 端口号
+        //   username: 'root',   // 用户名
+        //   password: 'root', // 密码
+        //   database:'test', //数据库名
+        //   timezone: '+08:00', //服务器上配置的时区
+        //   synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+        // }),
+      }),
+    ],
+  })
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'mysql',
+//       host: 'localhost',
+//       port: 3306,
+//       username: 'root',
+//       password: 'root',
+//       database: 'test',
+//       entities: [],
+//       synchronize: true,
+//     }),
+//   ],
+// })
+export class ProjModule {}
